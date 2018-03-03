1f9bae69d74bc8cdd3f99302b0b1207f62671cce
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-03-03 19:29:51 +0800

    bugfix

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,14 +5,15 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 
 var child_process = require('child_process');
+var path = require('path');
 
 router.all('/hook', function (req, res) {
     logger.debug('hook in');
     logger.debug(req.body);
 
-    logger.debug(__dirname);
-
-    child_process.execFile('../load.sh', [], { shell: true }, function (error, stdout, stderr) {
+    logger.debug(path.resolve(__dirname, '../load.sh'));
+    var dir = path.resolve(__dirname, '..');
+    child_process.execFile(path.resolve(__dirname, '../load.sh'), [], { shell: true }, function (error, stdout, stderr) {
         if(error) {
             throw error;
         }
